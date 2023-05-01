a08004fe9e72c2629442c0ed6177ead55e57e15e
Nakahara <Daisuke.Nakahara@sony.com>
2023-05-01 14:36:36 -0700

    Initial Upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AITRIOS_Console_Mockup.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
