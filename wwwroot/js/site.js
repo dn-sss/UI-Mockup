99b268419185a2dcc4618adcb9f1733aeba30ffe
Nakahara <Daisuke.Nakahara@sony.com>
2023-05-02 14:09:54 -0700

    Ver 0.5

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,37 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function initCanvas(canvasId, imageName) {
 
-// Write your JavaScript code.
+    let canvas = document.getElementById(canvasId);
+    let ctx = canvas.getContext('2d');
+
+    var image = new Image();
+    image.onload = function (event) {
+        drawCanvas(this, canvasId);
+    };
+    image.src = imageName;
+
+    let overlayCanvas = document.getElementById(`${canvasId}Overlay`);
+    let overlayCtx = overlayCanvas.getContext('2d');
+    overlayCtx.font = '30px serif';
+    overlayCtx.lineWidth = 1;
+    overlayCtx.textBaseline = "bottom";
+    overlayCtx.fillStyle = "yellow";
+    overlayCtx.textAlign = "center";
+    overlayCtx.fillText("overlay sample", parseInt(overlayCanvas.width / 2), parseInt(overlayCanvas.height/2));
+}
+
+function drawCanvas(image, canvasId) {
+
+    let canvas = document.getElementById(canvasId);
+    let canvasCtx = canvas.getContext('2d');
+
+    let imageWidth = image.width;
+    let imageHeight = image.height;
+
+    canvasCtx.drawImage(image, 0, 0);
+}
+
+function processSliderInput(evt) {
+    // get label
+    let label = this.labels[0];
+    label.firstElementChild.innerText = evt.target.value;
+}
