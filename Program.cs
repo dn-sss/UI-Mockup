eb454b49da05b80615624d6a737f503157cd0bc9
Nakahara <Daisuke.Nakahara@sony.com>
2023-05-09 15:10:02 -0700

    Add SignalR

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
+
+using AITRIOS_Console_Mockup.SignalR.Hubs;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("CoresPolicy", policy => { policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:8000").AllowCredentials(); });
+//});
+
+builder.Services.AddSignalR().AddAzureSignalR(builder.Configuration.GetSection("Azure")
+                                    .GetSection("SignalR")
+                                    .GetValue<string>("ConnectionString"));
 
 var app = builder.Build();
 
@@ -15,6 +27,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapHub<SignalRHub>("/hub");
 
 app.MapControllerRoute(
     name: "default",
